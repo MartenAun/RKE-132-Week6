e72271c498775d70d38e07280b6ee41f25a8bbe9
Marten. <marten.aun@tkt.ee>
2024-10-30 18:12:10 +0200

    add week6 code

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "Bananas";
+fruit[2] = "oranges";
+Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+int fruitarraylenght = fruit.Length;   
+Console.WriteLine($"There are {fruitarraylenght} in your array");
+for (int i = 0; i < fruitarraylenght; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+
+//foreach(string fruititem in fruit)
+//{
+//    Console.WriteLine(fruititem);
+//}
---
+++ Random element/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "Riis kanaga", "hiinakat", "wrapp", "vana villem", "pasta", "burks" };
+
+
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"We go eat {snacks[randomIndex]}");
